1c55b4d807ec0cbb85048197221785aae587b403
rtk0c <mail.tianyig@gmail.com>
2024-03-21 23:24:20 -0700

    RISC-V instruction encodings (R/I/S/U)

--- as/emit.h
+++ as/emit.h
@@ -8,7 +8,3 @@ typedef struct MSemitter {
 } MSemitter;
 
 MSemitter MS_make_emitter();
-
-void MS_emit_add(MSemitter* e, MSreg dst, MSreg op1, MSreg op2);
-void MS_emit_addi(MSemitter* e, MSreg dst, MSreg op, MSimmediate imm);
-void MS_emit_sub(MSemitter* e, MSreg dst, MSreg op1, MSreg op2);
