9788dedca18be8ad76464e97d8ca2232c3d373ec
rtk0c <mail.tianyig@gmail.com>
2024-03-25 13:16:16 -0700

    [inclass] Emitter for traditional (delay slotful) branch instructions

--- as/emit.c
+++ as/emit.c
@@ -16,11 +16,14 @@ MSemitter MS_make_emitter() {
 #define PACK_BEGIN uint32_t _pack_val = 0; int _write_nth_bit = 31
 // TODO sanity check that v does not have more than `bits` bits
 #define PACK_BITS(v, bits) _pack_val |= (((uint32_t)v & MASK(bits)) << (_write_nth_bit - bits + 1)); _write_nth_bit -= bits
+
 // Known as "R types" in old educational MIPS manuals
 // These are instructions with 0 in the op field, and have 5-5-5-5-5 bit fields coming after that
-#define PACK_BITS_SPECIAL(rs, rt, rd, shamt, funct) PACK_BITS(0, 6); PACK_BITS(rs, 5); PACK_BITS(rt, 5); PACK_BITS(rd, 5); PACK_BITS(shamt, 5); PACK_BITS(funct, 5)
+#define DO_PACK_SPECIAL(rs, rt, rd, shamt, funct) PACK_BEGIN; PACK_BITS(0, 6); PACK_BITS(rs, 5); PACK_BITS(rt, 5); PACK_BITS(rd, 5); PACK_BITS(shamt, 5); PACK_BITS(funct, 5)
 // Known as "I types" in old educational MIPS manuals
-#define PACK_BITS_IMM(op, rs, rt, imm) PACK_BITS(op, 6); PACK_BITS(rs, 5); PACK_BITS(rt, 5); PACK_BITS(imm, 16);
+#define DO_PACK_IMM(op, rs, rt, imm) assert(imm >= INT16_MIN && imm <= INT16_MAX); PACK_BEGIN; PACK_BITS(op, 6); PACK_BITS(rs, 5); PACK_BITS(rt, 5); PACK_BITS(imm, 16)
+#define DO_PACK_J_TYPE(op, imm) assert(MS_INT_IN_RANGE(imm, 26)); PACK_BEGIN; PACK_BITS(op, 6); PACK_BITS(imm, 26)
+#define DO_PACK_REGIMM(rs, code, imm) assert(imm >= INT16_MIN && imm <= INT16_MAX); PACK_BEGIN; PACK_BITS(0b000001, 6); PACK_BITS(rs, 5); PACK_BITS(code, 5); PACK_BITS(imm, 16)
 
 #if defined(__GNUC__) || defined(__clang__)
 #	define PACK_GET_VALUE ({ assert(_write_nth_bit == 0); _pack_val; })
@@ -28,32 +31,25 @@ MSemitter MS_make_emitter() {
 #	define PACK_GET_VALUE _pack_val
 #endif
 
+// Add Word (trapping)
 void MS_emit_add(MSemitter* e, MSreg dst, MSreg op1, MSreg op2) {
-	PACK_BEGIN;
-	PACK_BITS_SPECIAL(op1, op2, dst, 0, 0b100000);
+	DO_PACK_SPECIAL(op1, op2, dst, 0, 0b100000);
 	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
 }
 
+// Add Word (non-trapping)
 void MS_emit_addu(MSemitter* e, MSreg dst, MSreg op1, MSreg op2) {
-	PACK_BEGIN;
-	PACK_BITS_SPECIAL(op1, op2, dst, 0, 0b100001);
+	DO_PACK_SPECIAL(op1, op2, dst, 0, 0b100001);
 	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
 }
 
-void MS_emit_addi(MSemitter* e, MSreg dst, MSreg op, MSimmediate imm) {
-	assert(imm >= INT16_MIN && imm <= INT16_MAX);
-	PACK_BEGIN;
-	PACK_BITS_IMM(0b001000, op, dst, imm);
-	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
-}
-
+// Add Immediate (unsigned - non-trapping)
 void MS_emit_addiu(MSemitter* e, MSreg dst, MSreg op, MSimmediate imm) {
-	assert(imm >= INT16_MIN && imm <= INT16_MAX);
-	PACK_BEGIN;
-	PACK_BITS_IMM(0b001001, op, dst, imm);
+	DO_PACK_IMM(0b001001, op, dst, imm);
 	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
 }
 
+// Add Immediate to PC (unsigned - non-trapping)
 void MS_emit_addiupc(MSemitter* e, MSreg dst, MSimmediate imm) {
 	assert(imm >= INT16_MIN && imm <= INT16_MAX);
 	PACK_BEGIN;
@@ -64,33 +60,54 @@ void MS_emit_addiupc(MSemitter* e, MSreg dst, MSimmediate imm) {
 	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
 }
 
-// TODO should we keep this? or write the BGEZAL and warn on arguments that doesn't match the BAL special case
-void MS_emit_bal(MSemitter* e, MSimmediate offset) {
-	assert(imm >= INT16_MIN && imm <= INT16_MAX);
-	PACK_BEGIN;
-	PACK_BITS_IMM(0b000001, 0b00000, 0b10001, imm);
-	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
-}
-
-// TODO BALC
-
-// TODO are these deprecated?
-void MS_emit_beq(MSemitter* e, MSreg a, MSreg b, MSimmediate offset) {
-	assert(imm >= INT16_MIN && imm <= INT16_MAX);
-	PACK_BEGIN;
-	PACK_BITS_IMM(0b000100, a, b, imm);
-	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
-}
-
-void MS_emit_bne(MSemitter* e, MSreg a, MSreg b, MSimmediate offset) {
-	assert(imm >= INT16_MIN && imm <= INT16_MAX);
-	PACK_BEGIN;
-	PACK_BITS_IMM(0b000100, a, b, imm);
-	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
-}
-
+// SUBtract
 void MS_emit_sub(MSemitter* e, MSreg dst, MSreg op1, MSreg op2) {
-	PACK_BEGIN;
-	PACK_BITS_SPECIAL(op1, op2, dst, 0, 0b100000);
+	DO_PACK_SPECIAL(op1, op2, dst, 0, 0b100000);
 	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
 }
+
+// TODO should we keep this? or write the BGEZAL and warn on arguments that doesn't match the BAL special case
+// Branch And Link
+void MS_emit_bal(MSemitter* e, MSimmediate offset) {
+	DO_PACK_IMM(0b000001, 0b00000, 0b10001, offset);
+	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
+}
+
+// Branch And Link Compact
+void MS_emit_balc(MSemitter* e, MSimmediate offset) {
+	DO_PACK_J_TYPE(0b111010, offset);
+	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
+}
+
+// Branch Compact
+void MS_emit_bc(MSemitter* e, MSimmediate offset) {
+	DO_PACK_J_TYPE(0b110010, offset);
+	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
+}
+
+// Branch on EQuals
+void MS_emit_beq(MSemitter* e, MSreg a, MSreg b, MSimmediate offset) {
+	DO_PACK_IMM(0b000100, a, b, offset);
+	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
+}
+
+// Branch on Not Equals
+void MS_emit_bne(MSemitter* e, MSreg a, MSreg b, MSimmediate offset) {
+	DO_PACK_IMM(0b000101, a, b, offset);
+	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
+}
+
+// Branch on Greater Than or Equal to Zero
+void MS_emit_bgez(MSemitter* e, MSreg rs, MSimmediate offset) {
+	DO_PACK_REGIMM(rs, 0b00001, offset);
+	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
+}
+
+// Branch on Less Than Zero
+void MS_emit_bltz(MSemitter* e, MSreg rs, MSimmediate offset) {
+	DO_PACK_REGIMM(rs, 0b00000, offset);
+	MS_buf_append_u32(&e->instructions, PACK_GET_VALUE);
+}
+
+// TODO B{LE,GE,GT,LT,EQ,NE}ZALC
+// TODO B<cond>C
--- common/common.h
+++ common/common.h
@@ -7,3 +7,9 @@
 #define MS_TB(x) (((uint64_t)x) << 40)
 
 #define MS_ARRAY_SIZE(arr) sizeof(arr)/sizeof(arr[0])
+
+/// N-bit signed integer maximum value
+#define MS_INT_MAX_N(n) ((1 << (n - 1)) - 1)
+/// N-bit signed integer minimum value
+#define MS_INT_MIN_N(n) (-MS_INT_MAX_N(n) - 1)
+#define MS_INT_IN_RANGE(v, nbits) ((v) >= MS_INT_MIN_N(nbits) && (v) <= MS_INT_MAX_N(nbits))
